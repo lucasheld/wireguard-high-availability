0fe67c8b048ef7158177cf0fddb59086a1647c54
lucasheld <lucasheld@hotmail.de>
2022-05-15 22:08:16 +0200

    inital commit

---
+++ check_other_servers.py
@@ -0,0 +1,48 @@
+import requests
+from hcloud import Client
+
+
+API_TOKEN = "ZUmwoCyaSyHzoYbnjtDMecS1rLXXaboLJ4ZIppaVQJjbKInvty5O82V7KlHra3AS"
+FLOATING_IP_NAME = "wg-floating"
+OTHER_SERVER_IPS = ["49.12.232.10", "78.47.156.66"]
+
+
+def get_server_by_ip(client, ip):
+    servers = client.servers.get_all()
+    for server in servers:
+        if server.data_model.public_net.ipv4.ip == ip:
+            return server
+
+
+def is_healthy(ip):
+    url = f"http://{ip}"
+    r = requests.get(url)
+    return r.ok
+
+
+def get_healthy_server(client, current_ip):
+    for new_ip in OTHER_SERVER_IPS:
+        if new_ip != current_ip and is_healthy(new_ip):
+            return get_server_by_ip(client, new_ip)
+
+
+def main():
+    client = Client(token=API_TOKEN)
+
+    floating_ip = client.floating_ips.get_by_name(FLOATING_IP_NAME)
+    current_ip = floating_ip.data_model.ip
+
+    if is_healthy(current_ip):
+        print(f"current server healthy")
+    else:
+        print(f"current server unhealthy")
+        server = get_healthy_server(client, current_ip)
+        if server:
+            client.floating_ips.assign(floating_ip, server)
+            print(f"floating ip assigned to server {server.data_model.name}")
+        else:
+            print("no healthy server available")
+
+
+if __name__ == "__main__":
+    main()
---
+++ health_check.py
@@ -0,0 +1,32 @@
+import subprocess
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+INTERFACE_NAME = "wg0"
+PORT = 80
+
+
+def check_running():
+    try:
+        subprocess.check_output(f"ip link show {INTERFACE_NAME} up", shell=True, stderr=subprocess.DEVNULL)
+        return True
+    except:
+        return False
+
+
+class CustomRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        running = check_running()
+        if running:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"healthy")
+        else:
+            self.send_response(503)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"unhealthy")
+
+
+server = HTTPServer(('', PORT), CustomRequestHandler)
+server.serve_forever()
